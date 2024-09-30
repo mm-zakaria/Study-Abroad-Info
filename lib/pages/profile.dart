588929cee4792d25bb250294b5e187e504d2d97f
mm-zakaria <mmzakarianwucse103@gmail.com>
2024-10-01 04:29:08 +0600

    some changes

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -8,63 +8,65 @@ class Profile extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40.0,
-            ),
-            CircleAvatar(
-              radius: 80,
-              backgroundImage: AssetImage("assets/boy.jpeg"),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 5),
-                    color: Colors.orange.withOpacity(.2),
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40.0,
               ),
-              child: ListTile(
-                title: Text("Name"),
-                subtitle: Text("M M ZAKARIA"),
-                leading: Icon(Icons.person_3_sharp),
-                trailing: Icon(
-                  Icons.arrow_circle_right,
-                  color: Colors.grey,
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage("assets/boy.jpeg"),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 5),
+                      color: Colors.orange.withOpacity(.2),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                    ),
+                  ],
                 ),
-                tileColor: Colors.white,
+                child: ListTile(
+                  title: Text("Name"),
+                  subtitle: Text("M M ZAKARIA"),
+                  leading: Icon(Icons.person_3_sharp),
+                  trailing: Icon(
+                    Icons.arrow_circle_right,
+                    color: Colors.grey,
+                  ),
+                  tileColor: Colors.white,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            itemprofile("Contact Number", "01779587675", Icons.phone),
-            SizedBox(
-              height: 20.0,
-            ),
-            itemprofile("Address", "24/4 KDA AVE KHULNA", Icons.home),
-            SizedBox(
-              height: 20.0,
-            ),
-            itemprofile(
-                "Email", "mmzakarianwucse103@gmail.com", Icons.mail_rounded),
-                 SizedBox(
-              height: 20.0,
-            ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ElevatedButton(onPressed: (){}, child: Text("Edit Profile",style: TextStyle(color: Colors.green),)),
-                )
-          ],
+              SizedBox(
+                height: 20.0,
+              ),
+              itemprofile("Contact Number", "01779587675", Icons.phone),
+              SizedBox(
+                height: 20.0,
+              ),
+              itemprofile("Address", "24/4 KDA AVE KHULNA", Icons.home),
+              SizedBox(
+                height: 20.0,
+              ),
+              itemprofile(
+                  "Email", "mmzakarianwucse103@gmail.com", Icons.mail_rounded),
+                   SizedBox(
+                height: 20.0,
+              ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ElevatedButton(onPressed: (){}, child: Text("Edit Profile",style: TextStyle(color: Colors.green),)),
+                  )
+            ],
+          ),
         ),
       ),
     );
